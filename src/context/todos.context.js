3a89412f60583933f6fb4229f3131b13fb8feb93
ninohero <ninohero@muzocalo.com>
2020-09-25 14:28:53 -0400

    stopped excess rerenders

--- src/context/todos.context.js
+++ src/context/todos.context.js
@@ -1,6 +1,6 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext } from "react";
+import { useLocalStorageReducer } from "../hooks/useLocalStorageReducer";
 import todoReducer from "../reducers/Todo.reducer";
-import useTodoStates from "../hooks/useTodoState";
 const defaultTodos = [
   { id: 1, task: "Mow lawn", completed: false },
   { id: 2, task: "Release bugs", completed: true },
@@ -9,7 +9,11 @@ export const TodosContext = createContext();
 export const DispatchContext = createContext();
 
 export const TodosProvider = (props) => {
-  const [todos, dispatch] = useReducer(todoReducer, defaultTodos);
+  const [todos, dispatch] = useLocalStorageReducer(
+    "todos",
+    defaultTodos,
+    todoReducer
+  );
   return (
     <TodosContext.Provider value={todos}>
       <DispatchContext.Provider value={dispatch}>
